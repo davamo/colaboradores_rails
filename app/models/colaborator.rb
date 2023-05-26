c908f58c14a17e7b16332527bd0ca8372af0db35
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-26 11:54:20 -0400

    Feat: project colaboradores_rails

---
+++ app/models/colaborator.rb
@@ -0,0 +1,3 @@
+class Colaborator < ApplicationRecord
+    include Pagy::Frontend
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+
+  resources :colaborators
+
+  
+  root "colaborators#index"
+
+end
